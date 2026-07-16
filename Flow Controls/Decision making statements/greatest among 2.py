53fcb44c8b44dda5d2c2e46bfd9e3e6325570f8c
Anusha VK <anushavk851@gmail.com>
2026-07-16 11:45:14 +0530

    Implement logic to find greatest among two numbers

---
+++ Flow Controls/Decision making statements/greatest among 2.py
@@ -0,0 +1,7 @@
+#Take 2 numbers from user and find the greatest among 2
+  num1=int(input("enter a number: "))
+  num2=int(input("enter another number: "))
+  if num1>num2:
+              print(" first number is greatest")
+  else:
+              print("second number is greatest")
