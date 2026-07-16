22ba77f9c3990d9df38ae99dc7d741fecc0ff558
Anusha VK <anushavk851@gmail.com>
2026-07-16 11:46:33 +0530

    Implement login system with username and password

---
+++ Flow Controls/Decision making statements/login system.py
@@ -0,0 +1,7 @@
+#Login system
+  username=input("enter your username: ")
+  password=input("enter your password: ")
+  if (username=="anu@123" and password== "2001anu"):
+      print("logged in")
+  else:
+      print("invalid username or password")
