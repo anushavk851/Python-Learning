7e2270a033120f523be0fc4712180cf8006544ed
Anusha VK <anushavk851@gmail.com>
2026-07-16 10:11:53 +0530

    arithmetic operations for two numbers

---
+++ operators/arithmetic operator/display math operations.py
@@ -0,0 +1,8 @@
+#Write a program to input two numbers and display:sum,difference,product and division
+  n1=int(input("enter a number: "))
+  n2=int(input("enter another number: "))
+  sum=n1+n2
+  difference=n1-n2
+  product=n1*n2
+  divison=n1/n2
+  print("sum :",sum,"  product: ",product," difference: ", difference," division: ", divison)
