398ce105595d7c575d7e7117c3040ad2a9b04f62
Anusha VK <anushavk851@gmail.com>
2026-07-16 18:34:39 +0530

    Implement temperature decision-making logic

---
+++ Flow Controls/Decision making statements/temperature.py
@@ -0,0 +1,8 @@
+#If temperature is greater than 30, its a hot day.otherwise if its's less than 10 it's a cold day otherwise its neither hot nor cold.
+  temp= int(input("temp "))
+  if temp>= 30:
+      print("its's a hot day")
+  elif temp<10:
+      print("it's a cold day")
+  else:
+      print("it's neither hot nor cold")
