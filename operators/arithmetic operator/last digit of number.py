a908de7b065413f8557072c62af69a9ee0a79362
Anusha VK <anushavk851@gmail.com>
2026-07-16 10:02:50 +0530

    last digit extraction from a number

---
+++ operators/arithmetic operator/last digit of number.py
@@ -0,0 +1,4 @@
+#find last digit of a number (if n is 567 output have to be 7)
+  n=int(input("enter a three digit number:" ))
+  last_digit_number=n%10
+  print("last digit of the number is",last_digit_number)
