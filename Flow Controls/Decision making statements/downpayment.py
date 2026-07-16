e9ac08a88a93a57113317a0927ec58d619b43d67
Anusha VK <anushavk851@gmail.com>
2026-07-16 18:33:43 +0530

    downpayment calculation based on credit

---
+++ Flow Controls/Decision making statements/downpayment.py
@@ -0,0 +1,11 @@
+#price of a house is 1M.if a buyer has good credit,they need to put down 10% otherwise they need to put down 20%
+ # print the downpayment
+
+  price = 1000000
+  has_good_credit= True
+  if has_good_credit:
+        price= price*(1/10)
+        print(price)
+  else :
+        price= price *(2/10)
+        print(price)
