b339f7a7af76943ca0ebbe6ae9ca9340eed4d826
Anusha VK <anushavk851@gmail.com>
2026-07-16 18:41:48 +0530

    Implement traffic light decision logic

---
+++ Flow Controls/Decision making statements/traffic light.py
@@ -0,0 +1,11 @@
+#Traffic Light
+  color=input("enter the traffic color: ")
+  color=color.lower()
+  if color=="red":
+      print("stop")
+  elif color=="yellow":
+          print("wait")
+  elif color=="green":
+          print("go")
+  else:
+          print("invalid color")
