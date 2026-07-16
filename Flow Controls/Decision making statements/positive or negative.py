fc79859f6b481066a49c49ac6625da51ac74f22a
Anusha VK <anushavk851@gmail.com>
2026-07-16 18:32:51 +0530

    code to check if number is positive, negative, or zero

---
+++ Flow Controls/Decision making statements/positive or negative.py
@@ -0,0 +1,8 @@
+#inpput a number and check whether it is positive,negative or zero
+  num1=int(input("enter a number: "))
+  if num1>0:
+              print("given number is positive")
+  elif num1<0:
+              print("given number is negative")
+  else:
+          print("given number is zero")
