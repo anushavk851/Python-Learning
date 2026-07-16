b820a9ee6d387e21bde5cf7cccfdc11c16c18453
Anusha VK <anushavk851@gmail.com>
2026-07-16 11:41:13 +0530

    even or odd number check

---
+++ Flow Controls/Decision making statements/even or odd.py
@@ -0,0 +1,6 @@
+#Take a number from the user and check if it is even
+  num=int(input("enter a number: "))
+  if num%2==0:
+          print("number is even")
+  else:
+          print("number is odd")
