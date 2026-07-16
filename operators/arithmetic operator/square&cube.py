71184281f16d1af7cf6e02196162a746bf015043
Anusha VK <anushavk851@gmail.com>
2026-07-16 10:14:29 +0530

    code to calculate square and cube

---
+++ operators/arithmetic operator/square&cube.py
@@ -0,0 +1,6 @@
+#write a program to find square and cube of a number
+  n1=int(input("enter a number: "))
+  square=n1**2
+  cube= n1**3
+  print("square of the given number is: ", square)
+  print("cube of the given number is: ", cube)
