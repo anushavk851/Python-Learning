ede2056c6c8df7b573e5706cefbc6ba7e8b5491e
Anusha VK <anushavk851@gmail.com>
2026-07-16 19:09:22 +0530

    code to print a hollow triangle pattern

---
+++ Flow Controls/Pattern Printing/hollow triangle.py
@@ -0,0 +1,13 @@
+#Print a hollow triangle
+ #   *        
+ #   *  *
+ #   *     *
+ #   * * * * *
+ n=int(input("enter a number: "))
+ for i in range(1,n+1):
+    for j in range(1,i+1):
+       if j==1 or i==n or i==j:
+             print("*",end=" ")
+       else:
+          print(" ",end=" ")
+    print()   
