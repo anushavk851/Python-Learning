9126e55990d5ff7a4f804117df93e1e368271bea
Anusha VK <anushavk851@gmail.com>
2026-07-16 11:01:07 +0530

    sorting and reversing for two lists

---
+++ Data Types/mutable/List/sorting and reversing.py
@@ -0,0 +1,8 @@
+#Sort in ascending and descending order
+  list1=[230,4000,12,67,9]
+  list2=["zebra","mango","orange","apple"]
+  list1.sort()
+  print(list1)
+  list2.sort()
+  list2.reverse()
+  print(list2)
