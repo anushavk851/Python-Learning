004cade41f1475c0aa903eddab9a36dfa4c242bc
Anusha VK <anushavk851@gmail.com>
2026-07-16 22:45:55 +0530

    Add function to determine even or odd number
    Implement a function to check if a number is even or odd.

---
+++ Functions/Method 1(no argu & no return)/even_odd.py
@@ -0,0 +1,9 @@
+#Write a function that reads a number from the user and prints whether it is even or odd.
+  def odd_even():
+      num=int(input("enter a number: "))
+      if num%2==0:
+          print("Even")
+      else:
+          print("odd")
+
+  odd_even()
