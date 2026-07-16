0bd09e7a2143d15d70c0769f592b945ebaac3fe2
Anusha VK <anushavk851@gmail.com>
2026-07-16 11:42:38 +0530

    positive number check in Python

---
+++ Flow Controls/Decision making statements/positive or not.py
@@ -0,0 +1,6 @@
+#Check whether a number is positive or not
+  num=int(input("enter a number: "))
+  if num>0:
+              print("number is positive")
+  else:
+              print("number is negative")
