010f12f20fbd22eb302d60cfde4982523eaa6ee5
Anusha VK <anushavk851@gmail.com>
2026-07-16 10:16:00 +0530

    number swapping without a third variable

---
+++ operators/arithmetic operator/swap 2 numbers.py
@@ -0,0 +1,7 @@
+#write a program to swap two numbers without using a third variable
+  n1=int(input("enter a number: "))
+  n2=int(input("enter another number: "))
+  n1=n1+n2
+  n2=n1-n2
+  n1=n1-n2
+  print("n1: ",n1,"and n2: ",n2)
