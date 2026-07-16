db96adbdebab52c02972dcfca1a424e1a1be6146
Anusha VK <anushavk851@gmail.com>
2026-07-16 09:31:32 +0530

    Add script to calculate average of three numbers

---
+++ operators/arithmetic operator/Average of 3 numbers.py
@@ -0,0 +1,6 @@
+#find average of 3 numbers 
+  n1=int(input("enter a number:" ))
+  n2=int(input("enter a number:" ))
+  n3=int(input("enter a number:" ))
+  n=(n1+n2+n3)/3
+  print("average of the given numbers is",n)
