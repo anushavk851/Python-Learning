288dcba5a3cf0aaaf6f1dad685e4601329f2bf97
Anusha VK <anushavk851@gmail.com>
2026-07-16 22:49:49 +0530

    Implement function to find largest of three numbers

---
+++ Functions/Method 1(no argu & no return)/largest of 3.py
@@ -0,0 +1,13 @@
+#Find the largest of 3 numbers.
+  def largest():
+      n1=int(input("enter a number: "))
+      n2=int(input("enter a number: "))
+      n3=int(input("enter a number: "))
+      if n1>=n2 and n1>=n3:
+          print(n1)
+      elif n2>=n1 and n2>=n3:
+          print(n2)
+      else:
+          print(n3)
+
+  largest()
