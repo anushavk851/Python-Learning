4c0f59104d470b0709a8478d8554b9720b278707
Anusha VK <anushavk851@gmail.com>
2026-07-16 10:13:27 +0530

    calculate remainder of two numbers

---
+++ operators/arithmetic operator/reminder.py
@@ -0,0 +1,5 @@
+#write a program to find reminder when one number is divided by another
+  n1=int(input("enter a number: "))
+  n2=int(input("enter another number: "))
+  reminder= n1%n2
+  print("reminder when n1 is divided by n2 is :",reminder)
