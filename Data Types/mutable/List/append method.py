83d7cef50987dc837a220b27657ac5de06981f07
Anusha VK <anushavk851@gmail.com>
2026-07-16 10:51:38 +0530

    code to take and store user names

---
+++ Data Types/mutable/List/append method.py
@@ -0,0 +1,6 @@
+#Take 3 names from user and store them using append()
+  name=[]
+  name.append(input("enter 1st number: "))
+  name.append(input("enter 2nd number: "))
+  name.append(input("enter 3rd number: "))
+  print(name)
