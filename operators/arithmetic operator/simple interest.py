89804e377b3b683a33cd77f9c5a32bc6dd11ebae
Anusha VK <anushavk851@gmail.com>
2026-07-16 10:15:16 +0530

    Add simple interest calculation program

---
+++ operators/arithmetic operator/simple interest.py
@@ -0,0 +1,6 @@
+#write a program to calculate simple interest(SI=pnr)
+  p=int(input("enter the initial amount: "))
+  n=int(input("enter the duration in years: "))
+  r=int(input("enter the rate of interest: "))
+  SI= p*n*r
+  print("simple interest is: ",SI)
