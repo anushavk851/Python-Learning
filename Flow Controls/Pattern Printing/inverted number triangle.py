9cb8961f0304151d7692af9f5c88997877aa8b3f
Anusha VK <anushavk851@gmail.com>
2026-07-16 19:12:25 +0530

    Enable inverted number triangle printing
    Uncommented the code to print an inverted number triangle.

---
+++ Flow Controls/Pattern Printing/inverted number triangle.py
@@ -0,0 +1,10 @@
+#Print a inverted number triangle.
+ #1 2 3 4
+ #1 2 3
+ #1 2 
+ #1  
+ n= int(input("enter the number of rows: "))
+ for i in range(n,0,-1):
+    for j in range(1,i+1):
+       print(j,end=" ")
+    print()
