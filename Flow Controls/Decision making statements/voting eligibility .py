d9762d18dc2ea520f6fec54e0a167a95a6e921eb
Anusha VK <anushavk851@gmail.com>
2026-07-16 11:43:36 +0530

    voting eligibility check based on age

---
+++ Flow Controls/Decision making statements/voting eligibility .py
@@ -0,0 +1,6 @@
+#check whether a person is eligible for voting
+  age=int(input("enter your age: "))
+  if age>=18:
+              print("eligible for voting")
+  else:
+              print("not eligible")
