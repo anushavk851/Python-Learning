5d1f0c7b82794169bac0d3ea355a6617360d5892
Anusha VK <anushavk851@gmail.com>
2026-07-17 19:42:20 +0530

    Enable product calculation for numbers from 1 to n
    Uncommented code to calculate the product of numbers from 1 to n.

---
+++ Flow Controls/While loop/product of numbers.py
@@ -0,0 +1,8 @@
+#Find product of numbers from 1 to n.
+n=int(input("enter a number: "))
+i=1
+product=1
+while i<=n:
+     product=product*i
+     i=i+1
+print("product of the given numbers is: ",product)
