db9eee39fd459950f1665eed462877ca16525ecb
Anusha VK <anushavk851@gmail.com>
2026-07-17 19:52:21 +0530

    while loop for printing limits

---
+++ Flow Controls/While loop/limits.py
@@ -0,0 +1,6 @@
+#Print from given lower limit to upper limit
+lower=int(input("enter the lower limit: "))
+upper=int(input("enter the upper limit: "))
+while lower<=upper:
+  print(lower)
+  lower=lower+1
