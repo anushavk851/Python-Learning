c9380f73316a73d094cf051f0e5578df141e35cd
Anusha VK <anushavk851@gmail.com>
2026-07-17 18:16:18 +0530

    Implement square function and input/output handling

---
+++ Functions/Method 3(with argu & return type)/square of number.py
@@ -0,0 +1,8 @@
+#Write a function that accepts a number and return its square.
+  def square(n):
+      sq=n**2
+      return sq
+
+  num=int(input("enter a value: "))
+  result=square(num)
+  print(result)
