21096fc78f49f6e825ad1be782a9159e275c85e7
Anusha VK <anushavk851@gmail.com>
2026-07-17 11:26:00 +0530

    Add prime number check functionality
    Implemented logic to check if a number is prime.

---
+++ Flow Controls/FOR loop/prime or not.py
@@ -0,0 +1,10 @@
+#Check whether a number is prime or not.
+  n=int(input("enter a number: "))
+  count=0
+  for i in range(1,n+1):
+      if n%i==0:
+      count=count+1
+  if count==2:
+  print("its a prime number")
+  else:
+  print("not prime number") *)
