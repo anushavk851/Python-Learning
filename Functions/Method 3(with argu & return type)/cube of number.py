58f1d3b9190eb7d074b40543feb390e6e717d728
Anusha VK <anushavk851@gmail.com>
2026-07-17 18:17:39 +0530

    Implement cube function and input/output handling

---
+++ Functions/Method 3(with argu & return type)/cube of number.py
@@ -0,0 +1,8 @@
+#Write a function that accepts a number and return its cube.
+  def cube(n):
+      cu=n**3
+      return cu
+
+  num=int(input("enter a value: "))
+  result=cube(num)
+  print(result)
