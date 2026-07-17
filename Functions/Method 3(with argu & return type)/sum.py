b7d1e874b34a95c8d5401bb44ea0e64a0a1686b9
Anusha VK <anushavk851@gmail.com>
2026-07-17 18:14:32 +0530

    Implement addition function and user input handling

---
+++ Functions/Method 3(with argu & return type)/sum.py
@@ -0,0 +1,9 @@
+#Write a function that accepts two numbers and returns their sum.
+  def addition(n1,n2):
+      sum=n1+n2
+      return sum
+
+  n1=int(input("enter a number: "))
+  n2=int(input("enter a number: "))
+  result=addition(n1,n2)
+  print(result)
