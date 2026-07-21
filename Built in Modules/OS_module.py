d5cddc85e29e8dccf2a44a4035a4e1139b0eb18f
Anusha VK <anushavk851@gmail.com>
2026-07-21 15:37:50 +0530

    Add OS module functionality for file operations
    Implemented basic file and directory operations using the os module.

---
+++ Built in Modules/OS_module.py
@@ -0,0 +1,41 @@
+import os
+#Operating module that lets program interact with operating system.
+
+# getcwd()--Returns current working directory.
+print(os.getcwd())
+
+# listdir()--Lists files and folders.
+print(os.listdir())
+
+# mkdir()--Creates a new directory.
+os.mkdir("DemoFolder")
+
+# makedirs()--Creates multiple directories.
+#os.makedirs("Folder1/Folder2")
+
+# rmdir()--Removes a directory.
+#os.rmdir("DemoFolder")
+
+# remove()--Deletes a file.
+#os.remove("sample.txt")
+
+# rename()--Renames a file or folder.
+#os.rename("old.txt", "new.txt")
+
+# chdir()--Changes current directory.
+#os.chdir(".")
+
+# path.exists()--Checks if a path exists.
+print(os.path.exists("."))
+
+# system()--Executes a system command.
+print("\n10. system()")
+if os.name == "nt":      # Windows
+    os.system("dir")
+else:                    # Linux/macOS
+    os.system("ls")
+
+
+
+
+
