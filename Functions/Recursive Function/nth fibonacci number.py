5fba10948137abbba542e3943626161a42961401
Anusha VK <anushavk851@gmail.com>
2026-07-22 23:03:27 +0530

    Add function to generate nth Fibonacci number

---
+++ Functions/Recursive Function/nth fibonacci number.py
@@ -0,0 +1,11 @@
+#Generate the nth fibonacci number.
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+n = 7
+print("Fibonacci:", fibonacci(n))
