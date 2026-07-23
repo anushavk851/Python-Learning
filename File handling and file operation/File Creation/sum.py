749d007a36d2df6ac321ac4e1c8a34aa20a954f5
Anusha VK <anushavk851@gmail.com>
2026-07-23 22:20:42 +0530

    Add sum.py for file handling and summation

---
+++ File handling and file operation/File Creation/sum.py
@@ -0,0 +1,15 @@
+# f=open("numbers.txt","x")
+# f.close()
+
+# with open("numbers.txt","a") as f:
+#     f.write("12")
+#     f.write("\n13")
+#     f.write("\n14")
+
+#create a list by adding the numbers in file then find its sum
+    # f=open("numbers.txt","r")
+    # lst=[]
+    # for i in f:
+    #     lst.append(int(i.rstrip()))
+    # print(lst)
+    # print(sum(lst))             #sum is a function
