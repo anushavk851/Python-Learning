918f989b594a0f1f430bb710d0f480e1bed18e9b
Anusha VK <anushavk851@gmail.com>
2026-07-25 22:00:14 +0530

    Add menu-driven math operations program
    Implemented a menu-driven program for mathematical operations using the math module, including square root, power, and factorial.

---
+++ Built in Modules/problems/problem 1.py
@@ -0,0 +1,33 @@
+#Write a Python program using the math module.
+ # Problem
+ # Display a menu:
+    # 1. Square Root
+    # 2. Power
+    # 3. Factorial
+    # 4. Exit
+ # Take the user's choice and perform the corresponding operation using functions from the math module.
+
+    # from math import sqrt,pow,factorial
+    # while True:
+    #     print("menu")
+    #     print("1.square root")
+    #     print("2.power")
+    #     print("3.factorial")
+    #     print("4.exit")
+
+    #     user_choice=int(input("enter your choice from the menu : "))
+
+    #     if user_choice==1:
+    #         num=int(input("enter the number in which you have to perform operation: "))
+    #         print("square root of given number is: ",sqrt(num))
+    #     elif user_choice==2:
+    #         num=int(input("enter the number in which you have to perform operation: "))
+    #         power=int(input("enter the power: "))
+    #         print("power of given number: ",pow(num,power))
+    #     elif user_choice==3:
+    #         num=int(input("enter the number in which you have to perform operation: "))
+    #         print("factorial of given number is: ",factorial(num))
+    #     elif user_choice==4:
+    #         break
+    #     else:
+    #         print("invalid choice please choose again")
