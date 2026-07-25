ec4c44e1ca2afe667909e5265b86f19783d34157
Anusha VK <anushavk851@gmail.com>
2026-07-25 23:13:29 +0530

    Implement file operations for student marks

---
+++ File handling and file operation/File Creation/students_mark.py
@@ -0,0 +1,41 @@
+#2 Create a file named marks.txt.
+ # Store 5 marks entered by the user into the file.
+ # Then read the file and display:
+    # Total marks
+    # Average marks
+    # Highest mark
+    # Lowest mark
+ # Requirements:
+    # Use file handling (open, write, read/readlines, close).
+    # Take the 5 marks from the user.
+    # Do not use exception handling.
+
+  with open("marks.txt","x") as f:
+      pass
+
+  with open("marks.txt","w") as f:
+      for i in range(5):
+          mark = input("Enter mark: ")
+          f.write(mark + "\n")
+
+  with open("marks.txt","r") as f:
+      lst=[]
+      for i in f:
+          lst.append(int(i.strip()))
+
+      total_mark=0
+      count=0
+      highest=lst[0]
+      lowest=lst[0]
+      for i in lst:
+          total_mark+=i
+          count=count+1
+      print("total mark is: ",total_mark)
+      print("average mark is: ",total_mark/count)
+      for i in lst:
+          if i>highest:
+              highest=i
+          if i<lowest:
+              lowest=i
+      print("highest mark is: ",highest)
+  #     print("lowest mark is: ",lowest)
